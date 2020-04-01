2171166125d73447d169d0256320f94f92203036
Pavan Kumar <pavancse17@gmail.com>
2020-04-01 19:17:45 +0530

    Updates

---
+++ precache-manifest.b97152e8d4beb04bdcde4827544dcd96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f19cd0ab9e2c18f1d0f3d474c3c648",
+    "url": "/twitter_streams_frontend/index.html"
+  },
+  {
+    "revision": "43264d883c8b21821c94",
+    "url": "/twitter_streams_frontend/static/css/2.ab8a0683.chunk.css"
+  },
+  {
+    "revision": "0e7a313ddd4667d18366",
+    "url": "/twitter_streams_frontend/static/css/main.58c2da8e.chunk.css"
+  },
+  {
+    "revision": "43264d883c8b21821c94",
+    "url": "/twitter_streams_frontend/static/js/2.931d31e4.chunk.js"
+  },
+  {
+    "revision": "10113189c9a52c3faf4b82fe91e82ff5",
+    "url": "/twitter_streams_frontend/static/js/2.931d31e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e7a313ddd4667d18366",
+    "url": "/twitter_streams_frontend/static/js/main.fd1ade43.chunk.js"
+  },
+  {
+    "revision": "b21c8e2d8e1f41babadf",
+    "url": "/twitter_streams_frontend/static/js/runtime-main.07385e6f.js"
+  }
+]);
